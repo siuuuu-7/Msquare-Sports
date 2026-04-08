3677e8b01d0984511f8fbcd80ea7fc6ae99366ed
siuuuu-7 <siuuslayerx@gmail.com>
2026-04-08 12:52:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -108,3 +108,6 @@ function refreshProducts() {
 
 // LOAD ON PAGE
 window.addEventListener("load", loadProducts);
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+const auth = getAuth(app);
