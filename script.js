4ea5be9e10fcdbed3117c35c2f03f90ace5e2ed9
siuuuu-7 <siuuslayerx@gmail.com>
2026-04-08 14:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyB9k-ns5ykMT9XroM9byq-rcIeCvEseRfY",
   authDomain: "msquare-sports.firebaseapp.com",
-  projectId: "msquare-sports",
+  projectId: "Msquare-Sports",
   storageBucket: "msquare-sports.firebasestorage.app",
   messagingSenderId: "144814096708",
   appId: "1:144814096708:web:8daff5c52f0c00d7a81711"
