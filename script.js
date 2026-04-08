98ef7e1842dbaf7480f1c8c508123c2bfa74271c
siuuuu-7 <siuuslayerx@gmail.com>
2026-04-08 14:44:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut }
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 // 🔥 FIREBASE CONFIG
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9k-ns5ykMT9XroM9byq-rcIeCvEseRfY",
+  apiKey: "AIzaSyBdcTPbkGt1tYJ5ZyUU1Sg8h1DhNaafTj8",
   authDomain: "msquare-sports.firebaseapp.com",
-  projectId: "Msquare-Sports",
+  projectId: "msquare-sports",
   storageBucket: "msquare-sports.firebasestorage.app",
   messagingSenderId: "144814096708",
-  appId: "1:144814096708:web:8daff5c52f0c00d7a81711"
+  appId: "1:144814096708:web:8daff5c52f0c00d7a81711",
+  measurementId: "G-BFZNTJJEEH"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
 let isAdmin = false;
 
@@ -29,8 +38,9 @@ window.loginAdmin = async function () {
     await signInWithEmailAndPassword(auth, email, password);
     alert("Login Successful");
   } catch (err) {
-  console.log(err);
-  alert(err.message);
+  console.log(err.code);
+  console.log(err.message);
+  alert(err.code);
 }
 };
 
