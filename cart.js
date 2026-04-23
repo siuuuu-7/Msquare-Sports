5de661e5389e096c27463a27cf220850c545518a
siuuuu-7 <siuuslayerx@gmail.com>
2026-04-23 18:21:44 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,36 +1,13 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+function addToCart(name, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function saveCart() {
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-window.addToCart = function (name, price, img = "") {
-  cart.push({ name, price, img });
-  saveCart();
-  alert("Added to cart");
-};
-
-function renderCart() {
-  const cartDiv = document.getElementById("cartItems");
-  if (!cartDiv) return;
-
-  cartDiv.innerHTML = "";
-
-  cart.forEach((item, i) => {
-    cartDiv.innerHTML += `
-      <div>
-        ${item.img ? `<img src="${item.img}" width="50">` : ""}
-        ${item.name} - ${item.price}
-        <button onclick="removeFromCart(${i})">X</button>
-      </div>
-    `;
+  cart.push({
+    name: name,
+    price: price,
+    qty: 1
   });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert(name + " added to cart 🛒");
 }
-
-window.removeFromCart = function (i) {
-  cart.splice(i, 1);
-  saveCart();
-  renderCart();
-};
-
-renderCart();
